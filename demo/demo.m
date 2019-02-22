ecd4b6a076ea7d692dc18059a112b59735b0b377
QGrain <zhiyuzhang1999@163.com>
2019-02-23 00:53:31 +0800

    renew a bit

---
+++ demo/demo.m
@@ -0,0 +1,23 @@
+% author zzy
+close all;clear all;clc;
+
+audio = [];
+
+sample_rate = 40000; % 2*20kz
+ref_note = 261.6; % base freq for middle C
+beat_num = 120; % 120 beats per minute
+beat_len = 60 / beat_num; % length of one note
+
+
+t = [0:1/sample_rate:beat_len-1/sample_rate];
+envelope1 = exp(-5.*t);
+% envelope2 = sin(2*pi.*t);
+series = [0 2 4 5 7 9 11 12];
+
+for i = [0 2 4 5 7 9 11 12]
+    freq = ref_note * 2^( i/12 );
+    y = sin(2*pi*freq*t).*envelope1;
+    audio = [audio y];
+end
+
+sound(audio, sample_rate);
