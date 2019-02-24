5839c041a25b1d19c82159ed29103ae30c12bc8b
QGrain <zhiyuzhang1999@163.com>
2019-02-24 23:21:56 +0800

    test the demo

--- demo/demo.m
+++ demo/demo.m
@@ -6,11 +6,11 @@ audio = [];
 sample_rate = 40000; % 2*20kz
 ref_note = 261.6; % base freq for middle C
 beat_num = 120; % 120 beats per minute
-beat_len = 60 / beat_num; % length of one note
+beat_len = 60 / beat_num; % length of one beat
 
 
 t = [0:1/sample_rate:beat_len-1/sample_rate];
-envelope1 = exp(-5.*t);
+envelope1 = exp(-10.*t);
 % envelope2 = sin(2*pi.*t);
 series = [0 2 4 5 7 9 11 12];
 
@@ -19,5 +19,7 @@ for i = [0 2 4 5 7 9 11 12]
     y = sin(2*pi*freq*t).*envelope1;
     audio = [audio y];
 end
+
+plot(audio);
 
 sound(audio, sample_rate);
