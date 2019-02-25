9af6f2d1f2dbd293b1dd2eb136b407fe6a3d9665
Leegenux <qq1350747952@163.com>
2019-02-25 22:38:08 +0800

    Finish the raw audio file processing API

---
+++ instrumentPropertyScan.m
@@ -0,0 +1,47 @@
+function [harm_coef, avg_envelope, one_sec_index] = instrumentPropertyScan(filename)
+    % Get the sample array and sampling frequency.
+    [audio, one_sec_index] = audioread(filename);
+    audio = audio(:,1); % Get data of only one tunnel
+
+    % Get the standarized envelope
+    [upper_envelope, lower_envelope] = envelope(audio);
+    avg_envelope = (upper_envelope - lower_envelope);
+    avg_envelope = avg_envelope / max(avg_envelope); % Standarization
+    
+    % Fourier Transformation
+    fs = one_sec_index;
+    Y = fft(audio); % Y are complexes
+    L = length(audio);
+    f = fs*(0:(L/2))/L;
+    P2 = abs(Y)/L;  % Modulus of complexes
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    
+    % Find the optimal distance
+    [maxValue, index] = max(P1);
+	dist = f(ceil(index*0.9));
+    
+    % Find the possible harmonics
+    [peaks, oriLocs] = findpeaks(P1, f, 'MinPeakDistance', dist);
+    
+    % Transpose
+    size_peaks = size(peaks);
+    if size_peaks(1) == 1
+        peaks = transpose(peaks);
+    end
+    size_oriLocs = size(oriLocs);
+    if size_oriLocs(1) == 1
+        oriLocs = transpose(oriLocs);
+    end
+
+    % Get the harmonics
+    peaks = peaks/maxValue;   % Standarization
+    toSortPeaks = horzcat(peaks, oriLocs);
+    sortedPeaks = transpose(sortrows(toSortPeaks, 1, 'descend'));
+    sortedL = length(sortedPeaks);
+    if sortedL > 6
+        sortedL = 6;
+    end
+    harm_coef = sortedPeaks(:, 1:sortedL);
+end
+
