1b57d801f72467d02f812eb8032423028c3fe504
QGrain <zhiyuzhang1999@163.com>
2019-02-25 11:24:10 +0800

    Add envelope parse function

---
+++ soundParse.m
@@ -0,0 +1,23 @@
+clear;
+clc;
+
+[y, Fs] = audioread('./sound_material/piano/piano_47.mp3');
+
+ylen = length(y);
+Nfft = 2^nextpow2(ylen(1)); %matlab 下标从1开始
+Y = fft(y, Nfft);
+f = Fs/2*linspace(0,1,Nfft/2+1);
+
+
+subplot(1,2,1);
+plot (y);
+title('raw sound of 47 piano');
+xlabel('time(ms)')
+
+subplot(1,2,2);
+plot(f, 2*abs(Y(1:Nfft/2+1)));
+title('freq after fft');
+xlabel('freq(Hz)');
+ylabel('|Y(f)|');
+
+sound (y, Fs);
