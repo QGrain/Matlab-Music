25a598d7841dea145c121d9f92603597aa1b82be
QGrain <zhiyuzhang1999@163.com>
2019-03-03 14:42:05 +0800

    update envelope parse function

--- soundParse.m
+++ soundParse.m
@@ -8,14 +8,25 @@ Nfft = 2^nextpow2(ylen(1)); %matlab 下标从1开始
 Y = fft(y, Nfft);
 f = Fs/2*linspace(0,1,Nfft/2+1);
 
+[yuper, ylower] = envelope(y);
+subplot(2,2,1)
+plot(yuper);
+title('yuper');
+xlabel('freq?');
 
-subplot(1,2,1);
+subplot(2,2,2);
+plot(ylower);
+title('ylower');
+xlabel('freq?');
+
+subplot(2,2,3);
 plot (y);
 title('raw sound of 47 piano');
 xlabel('time(ms)')
 
-subplot(1,2,2);
+subplot(2,2,4);
 plot(f, 2*abs(Y(1:Nfft/2+1)));
+%plot(f, Y(1:Nfft/2+1));
 title('freq after fft');
 xlabel('freq(Hz)');
 ylabel('|Y(f)|');
