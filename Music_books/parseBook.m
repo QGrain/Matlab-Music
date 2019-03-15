5ee873b38158bb06db3afa8a69ad87e9f27df982
QGrain <zhiyuzhan1999@163.com>
2019-03-15 16:09:15 +0800

    add key, isChord as return values

--- Music_books/parseBook.m
+++ Music_books/parseBook.m
@@ -35,27 +35,37 @@ function [basic_info book_single_beat] = parseBook(filename)
     book_cell = regexp(book, ' ', 'split');
 
     for j = 1:length(book_cell)
-        book_single_beat(j).key = transformKey(book_cell{j}(1), book_cell{j}(2), book_cell{j}(3));
+        [key envType] = transformKey(book_cell{j}(1), book_cell{j}(2), book_cell{j}(3));
+        book_single_beat(j).key = key;
+        book_single_beat(j).envType = envType;
+        book_single_beat(j).isChord = isChord(book_cell{j}(4));
         book_single_beat(j).time = book_cell{j}(5);
     end
     
     fclose(fid);
 end
 
-function key = transformKey(note_base, note_offset, scale)
-    ref_freq = 261.63; % freq for C-4
-    
-    ref_scale = 12 * 4 + noteScale('C', '-');
-    note_scale = noteScale(note_base, note_offset);
-    absolute_scale = 12 * (scale-'0') + note_scale;
-    
-    key = ref_freq * 2^((absolute_scale - ref_scale)/12);
+function [key envType] = transformKey(note_base, note_offset, scale)
     if note_base == 'N'
         key = 0;
+        envType = 0;
+    else
+        ref_freq = 261.63; % freq for C-4
+        
+        ref_scale = noteScale('C', '-', '4');
+        absolute_scale = noteScale(note_base, note_offset, scale);
+        key = ref_freq * 2^((absolute_scale - ref_scale)/12);
+        
+        minus = absolute_scale - noteScale('C', '-', scale);
+        if minus < 6
+            envType = (scale-'0') * 2 + 1;
+        else
+            envType = (scale-'0') * 2 + 2;
+        end
     end
 end
 
-function note_scale = noteScale(note_base, note_offset)
+function note_scale = noteScale(note_base, note_offset, scale)
     switch (note_base)
         case 'C'
             note_scale = 1;
@@ -80,5 +90,15 @@ function note_scale = noteScale(note_base, note_offset)
     end
     if note_offset == 'b'
         note_scale = note_scale - 1;
+    end
+    
+    note_scale = note_scale + 12 * (scale-'0');
+end
+
+function is_chord = isChord(label)
+    if label == '_'
+        is_chord = 0;
+    else
+        is_chord = 1;
     end
 end
